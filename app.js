d6b72c5d9a59d8682ec947223ed8fe24e0af3a7b
CreativeGhalib <creativeghalib@gmail.com>
2026-02-02 20:30:49 +0600

    function task

---
+++ app.js
@@ -0,0 +1,25 @@
+// let x = 12;
+// let y = 23;
+// if (y > x) {
+
+//   console.log("boro");
+// }
+
+// Task-3:
+// Check whether a string contains all the vowels a, e, i, o, u
+
+// ?"will have to finish the task!!"let str = 'A quick fox jumps over a lazy dog';
+// newStr = str.toLowerCase;
+// let vowels="aeiou"
+// for (const v of str) {
+
+// }
+
+function square(number) {
+  console.log('value of the number parameter', number);
+  const borgo = number * number;
+  console.log('sqare of the given number is:', borgo);
+  return borgo;
+}
+let sqarenumbers = square(12);
+console.log(sqarenumbers);
