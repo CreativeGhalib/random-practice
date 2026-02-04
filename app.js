3834c359a25988eb48b83ba8e81e23666a2820ff
CreativeGhalib <creativeghalib@gmail.com>
2026-02-04 12:40:17 +0600

    task 1 js object task

--- app.js
+++ app.js
@@ -31,68 +31,77 @@
 // let tNum = totalNumber(5,5);
 // console.log(tNum);
 
-function sumOfnumbers(number) {
-  let sum = 0;
-  for (const number of numbers) {
-      console.log(number);
-  return sum;
-  };
+// function sumOfnumbers(number) {
+//   let sum = 0;
+//   for (const number of numbers) {
+//       console.log(number);
+//   return sum;
+//   };
 
 
-}
-const numbs = [54, 62, 12, 6];
-const sum = sumOfnumbers(numbs);
-console.log(`sum of number is:` sum);
+// }
+// const numbs = [54, 62, 12, 6];
+// const sum = sumOfnumbers(numbs);
+// console.log(`sum of number is:` sum);
 
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-function subtract(a, b) {
-  return a - b;
-}
+// function subtract(a, b) {
+//   return a - b;
+// }
 
-function multiply(a, b) {
-  return a * b;
-}
+// function multiply(a, b) {
+//   return a * b;
+// }
 
-function divide(a, b) {
-  return a / b;
-}
+// function divide(a, b) {
+//   return a / b;
+// }
 
-function remainder(a, b) {
-  return a % b;
-}
+// function remainder(a, b) {
+//   return a % b;
+// }
 
-function power(a, b) {
-  return a ** b;
-}
+// function power(a, b) {
+//   return a ** b;
+// }
 
-function maxOfTwo(a, b) {
-  return a > b ? a : b;
-}
+// function maxOfTwo(a, b) {
+//   return a > b ? a : b;
+// }
 
-function minOfTwo(a, b) {
-  return a < b ? a : b;
-}
+// function minOfTwo(a, b) {
+//   return a < b ? a : b;
+// }
 
-function average(a, b) {
-  return (a + b) / 2;
-}
+// function average(a, b) {
+//   return (a + b) / 2;
+// }
 
-function difference(a, b) {
-  return Math.abs(a - b);
-}
+// function difference(a, b) {
+//   return Math.abs(a - b);
+// }
 
-console.log(add(5, 5));
-console.log(subtract(10, 3));
-console.log(multiply(4, 6));
-console.log(divide(20, 4));
-console.log(remainder(17, 5));
-console.log(power(2, 8));
-console.log(maxOfTwo(9, 12));
-console.log(minOfTwo(9, 12));
-console.log(average(7, 13));
-console.log(difference(50, 18));
+// console.log(add(5, 5));
+// console.log(subtract(10, 3));
+// console.log(multiply(4, 6));
+// console.log(divide(20, 4));
+// console.log(remainder(17, 5));
+// console.log(power(2, 8));
+// console.log(maxOfTwo(9, 12));
+// console.log(minOfTwo(9, 12));
+// console.log(average(7, 13));
+// console.log(difference(50, 18));
 
+
+const colors = {
+  red: '#ff0000',
+  green: '#00ff00',
+  blue: '#0000ff',
+  'golden rod': '#daa520',
+};
+const outputValue=colors["golden rod"]
+console.log(outputValue);
