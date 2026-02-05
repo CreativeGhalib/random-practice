e7d52d46c9c1c07d4994ed2a728cc43a847a7e85
CreativeGhalib <creativeghalib@gmail.com>
2026-02-05 17:23:25 +0600

    sone with task 3

--- simplefunctiontask.js
+++ simplefunctiontask.js
@@ -10,13 +10,30 @@
 // Task-2
 // Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
 
-function oddEven(number) {
-  if (number%2===0) {
-    return number / 2;
-  } else {
-    return number * 2;
+// function oddEven(number) {
+//   if (number%2===0) {
+//     return number / 2;
+//   } else {
+//     return number * 2;
+//   }
+
+// }
+// const result=oddEven(7)
+// console.log(result);
+
+
+// Task-3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function make_avg(arr) {
+  let sum = 0;
+
+  for (const number of arr) {
+    sum = sum + number;
+
   }
+  return sum / arr.length;
 
 }
-const result=oddEven(6)
+const result = make_avg([1, 2, 3, 4]);
 console.log(result);
