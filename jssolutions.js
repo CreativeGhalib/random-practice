0bf0b7288bee8192161f64fd887a01fca0748c8a
CreativeGhalib <creativeghalib@gmail.com>
2026-02-09 19:33:48 +0600

    solving random 50 tasks one by one

---
+++ jssolutions.js
@@ -0,0 +1,40 @@
+// 🔢 A. Math & Basic Logic (1–8)
+
+// একটি সংখ্যা even না odd চেক করো
+
+
+// একটি সংখ্যা positive, negative না zero চেক করো
+
+
+// তিনটি সংখ্যার গড় (average) বের করো
+
+
+// Celsius থেকে Fahrenheit convert করো
+
+
+// Rectangle এর area বের করো
+
+
+// একটি সংখ্যা 3 এবং 5 উভয় দিয়ে divisible কিনা চেক করো
+
+
+
+// দুটি সংখ্যা যোগ করার একটি প্রোগ্রাম লেখো
+
+
+// function doSum(a, b) {
+//   return a + b;
+// }
+// let result = doSum(1, 2);
+// console.log(result);
+// const doSum = (a, b) => { return a + b };
+// let result = doSum(1, 2);
+// console.log(result);
+
+
+// দুটি সংখ্যার বিয়োগ, গুণ ও ভাগ বের করো
+const ultimateMath = (x, y) => {
+return `subtraction=${x-y} multiplication=${x*y} division=${x/y}`
+}
+const result = ultimateMath(12, 8);
+console.log(result)
