98da722b784041dfd10900919b735eff792ecd0d
CreativeGhalib <creativeghalib@gmail.com>
2026-02-14 22:07:19 +0600

    done with task 1 to 4 ,about to finish my practice after compliting th e 5 and 6 .

--- arrayLooping.js
+++ arrayLooping.js
@@ -7,7 +7,7 @@
 // alternative way
 
 
-const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 
 // const reversedArray = [];
 // for (let i = colors.length - 1; i >= 0; i--) {
@@ -33,6 +33,55 @@ const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 
 // Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
 
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const newArr = [];
+
+// for (const number of numbers) {
+
+
+//   if (number % 2 === 0) {
+//     newArr.push(number);
+//   }
+
+// }
+// console.log(newArr);
+
+
 // Output:
 
+
 // [12, 98, 76, 46]
+
+// Task 3
+// Use a for...of loop to concatenate all the elements of an array into a single string.
+
+const names = ['Tom', 'Tim', 'Tin', 'Tik'];
+let concatenated = '';
+for (const name of names) {
+  concatenated += name;
+}
+console.log(concatenated);
+
+
+// Output:
+
+// 'TomTimTinTik'
+// Task 4 (Hard)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+// Input: const statement = 'I am a hard working person'
+
+// Output:
+
+// 'person working hard a am I'
+
+
+
+const statement = 'I am a hard working person';
+const words = statement.split(' ');
+const reversedWords = [];
+for (let i = words.length - 1; i >= 0; i--) {
+  reversedWords.push(words[i]);
+}
+const reversedStatement = reversedWords.join(' ');
+console.log(reversedStatement);
