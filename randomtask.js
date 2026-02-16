e9a730b1c274020e2029b711dd7d7c194ad12484
CreativeGhalib <creativeghalib@gmail.com>
2026-02-16 22:03:15 +0600

    P01 Add Prop

---
+++ randomtask.js
@@ -0,0 +1,9 @@
+// 20 object problems (each includes a solution)
+
+// Problem-01: Create an object and add a new property.
+{
+  const person = { name: 'Ava', age: 21 };
+  person.city = 'Seattle';
+  console.log('P01', person);
+}
+
